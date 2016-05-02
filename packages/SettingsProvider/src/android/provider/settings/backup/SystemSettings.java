9c2c8f56b366e233931486d4ec47f64ab537a02a
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-05-02 09:34:21 -0500

    SystemUI: Fix toggling lockscreen rotation [1/3]
    Change-Id: I28fea0124c91b8efc206a940761e77fa61d1fefc
    [nurkeinneid] partial pick of LineageOS/android_frameworks_base@b5b859a5b292534ce9514b9a86f2e4d066c20a34
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -89,5 +89,6 @@ public class SystemSettings {
         Settings.System.NOTIFICATION_LIGHT_PULSE,
         Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD,
         Settings.System.ENABLE_FLOATING_ROTATION_BUTTON,
+        Settings.System.LOCKSCREEN_ROTATION,
     };
 }
