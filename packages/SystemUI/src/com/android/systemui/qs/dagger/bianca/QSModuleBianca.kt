0764e6ef5ff43b401b52b202449e2e599ba66e7e
Christian Oder <myself5@carbonrom.org>
2019-11-26 23:35:01 +0100

    SystemUI: Introduce Data Switch QS Tile
    * Based on OnePlus' OxygenOS tile, reworked to work with AOSP toggling without requirements on proprietary telephony-ext features
    
    idoybh (YAAP): Adapt to A12
    
    [ghostrider-reborn]
    - Set secondary label to SIM1 or SIM2 instead of just "On"
    
    [Jyotiraditya Panda <jyotiraditya@aospa.co>]
    - Code cleanup and improvements
    
    [SamarV-121]
    - Add getDefaultDataPhoneId() method here since it got removed in commit [1]
    
    - [1] 40dfa97
    
    Change-Id: Ie2e280c07f24f9da6b4ee218b72501a2713ce429
    Signed-off-by: Hernán Castañón Álvarez <herna@paranoidandroid.co>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    
    Change-Id: Ie2e280c07f24f9da6b4ee218b72501a2713ce429

--- packages/SystemUI/src/com/android/systemui/qs/dagger/bianca/QSModuleBianca.kt
+++ packages/SystemUI/src/com/android/systemui/qs/dagger/bianca/QSModuleBianca.kt
@@ -19,6 +19,7 @@ package com.android.systemui.qs.dagger.bianca
 import com.android.systemui.qs.tileimpl.QSTileImpl
 import com.android.systemui.qs.tiles.AutoBrightnessTile
 import com.android.systemui.qs.tiles.CaffeineTile
+import com.android.systemui.qs.tiles.DataSwitchTile
 import com.android.systemui.qs.tiles.HeadsUpTile
 import com.android.systemui.qs.tiles.RebootTile
 import com.android.systemui.qs.tiles.SyncTile
@@ -42,6 +43,12 @@ interface QSModuleBianca {
     @IntoMap
     @StringKey(UsbTetherTile.TILE_SPEC)
     fun bindUsbTetherTile(usbtetherTile: UsbTetherTile): QSTileImpl<*>
+
+    /** Inject DataSwitchTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(DataSwitchTile.TILE_SPEC)
+    fun bindDataSwitchTile(dataSwitchTile: DataSwitchTile): QSTileImpl<*>
 
     /** Inject HeadsUpTile into tileMap in QSModule */
     @Binds
