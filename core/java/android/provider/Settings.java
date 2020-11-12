a26db6937c08a330b2728f98f747bee65509e3e6
Dil3mm4 <dil3mm4.dev@gmail.com>
2020-11-12 14:30:29 +0000

    Feature: reticker [1/2]
    [someone5678]
    * Based on crDroid Android's cleaned A12 commit[1]
    * Forward port to Android 13
    * Follow the Material You styles
    
    Original commit:
    https://github.com/Descendant-XI/frameworks_base/commit/6ca0cff0da94d07a14ca8695b7bde5eaea9deef5
    
    Ref:
    [1] https://github.com/crdroidandroid/android_frameworks_base/commit/e44ede735e177d8297cfbfa8b22a21041c229ace
    
    Change-Id: I582d0484977f0e850d158e8645171e638fcc0c4c
    Signed-off-by: someone5678 <nemui3353@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5600,6 +5600,18 @@ public final class Settings {
         public static final String SCREENSHOT_SHUTTER_SOUND = "screenshot_shutter_sound";
 
         /**
+         * reTicker Status
+         * @hide
+         */
+        public static final String RETICKER_STATUS = "reticker_status";
+
+        /**
+         * reTicker Colored
+         * @hide
+         */
+        public static final String RETICKER_COLORED = "reticker_colored";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
