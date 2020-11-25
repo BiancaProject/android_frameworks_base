1cbada57cb9c9c3302a42b127f1bb61a110d223f
Genkzsz11 <genkzsz11@nusantaradevs.org>
2020-11-26 02:55:18 +0700

    Add partial screenshot action to navbar action [1/2]
    Signed-off-by: Genkzsz11 <genkzsz11@nusantaradevs.org>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6382,6 +6382,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case NavbarUtilities.KEY_ACTION_KILL_APP:
                 BiancaUtils.killForegroundApp();
                 break;
+            case NavbarUtilities.KEY_ACTION_PARTIAL_SCREENSHOT:
+                BiancaUtils.takeScreenshot(false);
+                break;
         }
     }
 
