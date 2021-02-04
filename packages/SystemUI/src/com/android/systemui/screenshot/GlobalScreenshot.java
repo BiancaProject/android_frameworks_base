01ad3814fafcba2409f3543ac30ef79ecd2b2592
Michael W <baddaemon87@gmail.com>
2021-02-04 17:49:58 +0100

    SystemUI: Screenshots: Refactor shutter sound logic
    * Commit 2f09bacae02fe7a99d62355331c3a26b83752a6a introduced some
      duplicated code
    * Move it to an own method to reduce the footprint in the AOSP code parts
      and to reduce duplication
    
    Change-Id: I57eaaee4db401d16cc6ef65c68604cdb4053ca01

--- packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -770,21 +770,7 @@ public class GlobalScreenshot implements ViewTreeObserver.OnComputeInternalInset
     private void saveScreenshotAndToast(Consumer<Uri> finisher) {
         // Play the shutter sound to notify that we've taken a screenshot
         mScreenshotHandler.post(() -> {
-            switch (mAudioManager.getRingerMode()) {
-                case AudioManager.RINGER_MODE_SILENT:
-                    // do nothing
-                    break;
-                case AudioManager.RINGER_MODE_VIBRATE:
-                    if (mVibrator != null && mVibrator.hasVibrator()) {
-                        mVibrator.vibrate(VibrationEffect.createOneShot(50,
-                                VibrationEffect.DEFAULT_AMPLITUDE));
-                    }
-                    break;
-                case AudioManager.RINGER_MODE_NORMAL:
-                    // Play the shutter sound to notify that we've taken a screenshot
-                    mCameraSound.play(MediaActionSound.SHUTTER_CLICK);
-                    break;
-            }
+            playShutterSound();
         });
 
         saveScreenshotInWorkerThread(finisher, new ActionsReadyListener() {
@@ -839,21 +825,7 @@ public class GlobalScreenshot implements ViewTreeObserver.OnComputeInternalInset
                     }
                 });
 
-                switch (mAudioManager.getRingerMode()) {
-                    case AudioManager.RINGER_MODE_SILENT:
-                        // do nothing
-                        break;
-                    case AudioManager.RINGER_MODE_VIBRATE:
-                        if (mVibrator != null && mVibrator.hasVibrator()) {
-                            mVibrator.vibrate(VibrationEffect.createOneShot(50,
-                                    VibrationEffect.DEFAULT_AMPLITUDE));
-                        }
-                        break;
-                    case AudioManager.RINGER_MODE_NORMAL:
-                        // Play the shutter sound to notify that we've taken a screenshot
-                        mCameraSound.play(MediaActionSound.SHUTTER_CLICK);
-                        break;
-                }
+                playShutterSound();
 
                 mScreenshotPreview.setLayerType(View.LAYER_TYPE_HARDWARE, null);
                 mScreenshotPreview.buildLayer();
@@ -1294,6 +1266,24 @@ public class GlobalScreenshot implements ViewTreeObserver.OnComputeInternalInset
                     new ColorDrawable(Color.BLACK), insetDrawable});
         } else {
             return insetDrawable;
+        }
+    }
+
+    private void playShutterSound() {
+        switch (mAudioManager.getRingerMode()) {
+            case AudioManager.RINGER_MODE_SILENT:
+                // do nothing
+                break;
+            case AudioManager.RINGER_MODE_VIBRATE:
+                if (mVibrator != null && mVibrator.hasVibrator()) {
+                    mVibrator.vibrate(VibrationEffect.createOneShot(50,
+                            VibrationEffect.DEFAULT_AMPLITUDE));
+                }
+                break;
+            case AudioManager.RINGER_MODE_NORMAL:
+                // Play the shutter sound to notify that we've taken a screenshot
+                mCameraSound.play(MediaActionSound.SHUTTER_CLICK);
+                break;
         }
     }
 }
