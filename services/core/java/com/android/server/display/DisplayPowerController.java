9a3e6b1a590c10e470970937a0129400cbd25c6d
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2021-05-14 00:53:09 +0200

    AutoBrightness: Add support for one shot auto-brightness
    - Only update auto brightness one time when screen is turned on.
    - Can be useful on devices where sensor is not accurate.
    
    Author: Eamon Powell <eamonpowell@outlook.com>
    Date:   Sun Aug 29 11:37:44 2021 +1000
    
        fixup! AutoBrightness: Add support for one shot auto-brightness:
    
        * Remove unnecessary newlines
        * Fix incorrect indentation
        * Fix typo in comments
    
        Change-Id: I7d6071f4344605b3dcc3f1c33ac4390f1020e03f
    
    Change-Id: I5187fdb7765550d4eaa999317ab95f487d44c8d8

--- services/core/java/com/android/server/display/DisplayPowerController.java
+++ services/core/java/com/android/server/display/DisplayPowerController.java
@@ -75,6 +75,8 @@ import com.android.server.display.whitebalance.DisplayWhiteBalanceFactory;
 import com.android.server.display.whitebalance.DisplayWhiteBalanceSettings;
 import com.android.server.policy.WindowManagerPolicy;
 
+import android.provider.Settings;
+
 import java.io.PrintWriter;
 import java.util.Objects;
 
@@ -483,6 +485,9 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
     private int mOnProximityPositiveMessages;
     private int mOnProximityNegativeMessages;
 
+    // Whether auto brightness is applied one shot when screen is turned on
+    private boolean mAutoBrightnessOneShot;
+
     // Animators.
     private ObjectAnimator mColorFadeOnAnimator;
     private ObjectAnimator mColorFadeOffAnimator;
@@ -543,6 +548,7 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
         mWindowManagerPolicy = LocalServices.getService(WindowManagerPolicy.class);
         mBlanker = blanker;
         mContext = context;
+        mAutoBrightnessOneShot = getAutoBrightnessOneShotSetting();
         mBrightnessTracker = brightnessTracker;
         // TODO: b/186428377 update brightness setting when display changes
         mBrightnessSetting = brightnessSetting;
@@ -958,6 +964,9 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
                 false /*notifyForDescendants*/, mSettingsObserver, UserHandle.USER_ALL);
         mContext.getContentResolver().registerContentObserver(
                 Settings.System.getUriFor(Settings.System.SCREEN_BRIGHTNESS_MODE),
+                false /*notifyForDescendants*/, mSettingsObserver, UserHandle.USER_ALL);
+        mContext.getContentResolver().registerContentObserver(
+                Settings.System.getUriFor(Settings.System.AUTO_BRIGHTNESS_ONE_SHOT),
                 false /*notifyForDescendants*/, mSettingsObserver, UserHandle.USER_ALL);
         handleBrightnessModeChange();
     }
@@ -1455,7 +1464,7 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
                     mLastUserSetScreenBrightness,
                     userSetBrightnessChanged, autoBrightnessAdjustment,
                     autoBrightnessAdjustmentChanged, mPowerRequest.policy,
-                    mShouldResetShortTermModel);
+                    mShouldResetShortTermModel, mAutoBrightnessOneShot);
             mShouldResetShortTermModel = false;
         }
 
@@ -2445,6 +2454,7 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
         // We don't bother with a pending variable for VR screen brightness since we just
         // immediately adapt to it.
         mScreenBrightnessForVr = getScreenBrightnessForVrSetting();
+        mAutoBrightnessOneShot = getAutoBrightnessOneShotSetting();
         sendUpdatePowerState();
     }
 
@@ -2458,6 +2468,12 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
                     == Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC;
             updatePowerState();
         });
+    }
+
+    private boolean getAutoBrightnessOneShotSetting() {
+        return Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.AUTO_BRIGHTNESS_ONE_SHOT,
+                0, UserHandle.USER_CURRENT) == 1;
     }
 
     private float getAutoBrightnessAdjustmentSetting() {
