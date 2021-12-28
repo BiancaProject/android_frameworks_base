2f5834d82e57a27f9a56aceccc7cad969eba2d4f
Anay Wadhera <awadhera@berkeley.edu>
2021-12-28 08:31:51 +0530

    SystemUI: Integrate smartspace and backgesture provider
    Change-Id: Iadaaa3e24b3a30edeff59e0224d063bf71e20d3d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: alanndz <alanndz7@gmail.com>

--- packages/SystemUI/src/com/bianca/systemui/BiancaSystemUIFactory.java
+++ packages/SystemUI/src/com/bianca/systemui/BiancaSystemUIFactory.java
@@ -1,12 +1,22 @@
 package com.bianca.systemui;
 
 import android.content.Context;
+import android.content.res.AssetManager;
+import android.os.Handler;
 
 import com.bianca.systemui.dagger.BiancaGlobalRootComponent;
+import com.bianca.systemui.dagger.BiancaSysUIComponent;
 import com.bianca.systemui.dagger.DaggerBiancaGlobalRootComponent;
 
 import com.android.systemui.SystemUIFactory;
 import com.android.systemui.dagger.GlobalRootComponent;
+import com.android.systemui.navigationbar.gestural.BackGestureTfClassifierProvider;
+import com.android.systemui.screenshot.ScreenshotNotificationSmartActionsProvider;
+
+import com.google.android.systemui.gesture.BackGestureTfClassifierProviderGoogle;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 
 public class BiancaSystemUIFactory extends SystemUIFactory {
     @Override
@@ -14,5 +24,18 @@ public class BiancaSystemUIFactory extends SystemUIFactory {
         return DaggerBiancaGlobalRootComponent.builder()
                 .context(context)
                 .build();
+    }
+
+    @Override
+    public BackGestureTfClassifierProvider createBackGestureTfClassifierProvider(AssetManager am, String modelName) {
+        return new BackGestureTfClassifierProviderGoogle(am, modelName);
+    }
+
+    @Override
+    public void init(Context context, boolean fromTest) throws ExecutionException, InterruptedException {
+        super.init(context, fromTest);
+        if (shouldInitializeComponents()) {
+            ((BiancaSysUIComponent) getSysUIComponent()).createKeyguardSmartspaceController();
+        }
     }
 }
