322ca780d7b66126ba0083b20ff230003c5494e2
Anay Wadhera <awadhera@berkeley.edu>
2021-12-28 08:31:51 +0530

    SystemUI: Integrate smartspace and backgesture provider
    Change-Id: Iadaaa3e24b3a30edeff59e0224d063bf71e20d3d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: alanndz <alanndz7@gmail.com>

--- packages/SystemUI/src/com/bianca/systemui/dagger/BiancaSysUIComponent.java
+++ packages/SystemUI/src/com/bianca/systemui/dagger/BiancaSysUIComponent.java
@@ -7,6 +7,9 @@ import com.android.systemui.dagger.SystemUIBinder;
 import com.android.systemui.dagger.SysUIComponent;
 import com.android.systemui.dagger.SystemUIModule;
 
+import com.bianca.systemui.keyguard.BiancaKeyguardSliceProvider;
+import com.bianca.systemui.smartspace.KeyguardSmartspaceController;
+
 import dagger.Subcomponent;
 
 @SysUISingleton
@@ -22,4 +25,12 @@ public interface BiancaSysUIComponent extends SysUIComponent {
     interface Builder extends SysUIComponent.Builder {
         BiancaSysUIComponent build();
     }
+
+    /**
+     * Member injection into the supplied argument.
+     */
+    void inject(BiancaKeyguardSliceProvider keyguardSliceProvider);
+
+    @SysUISingleton
+    KeyguardSmartspaceController createKeyguardSmartspaceController();
 }
