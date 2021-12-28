43f94d8a77d34991c34ee7e40986d1de6e4b163e
Anay Wadhera <awadhera@berkeley.edu>
2021-12-28 05:49:21 +0530

    SystemUI: Initial bianca SystemUI stub
    Change-Id: I67f36f20cc01dfc82572f2241287c3e739592306
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: alanndz <alanndz7@gmail.com>

---
+++ packages/SystemUI/src/com/bianca/systemui/dagger/BiancaSysUIComponent.java
@@ -0,0 +1,25 @@
+package com.bianca.systemui.dagger;
+
+import com.android.systemui.dagger.DefaultComponentBinder;
+import com.android.systemui.dagger.DependencyProvider;
+import com.android.systemui.dagger.SysUISingleton;
+import com.android.systemui.dagger.SystemUIBinder;
+import com.android.systemui.dagger.SysUIComponent;
+import com.android.systemui.dagger.SystemUIModule;
+
+import dagger.Subcomponent;
+
+@SysUISingleton
+@Subcomponent(modules = {
+        DefaultComponentBinder.class,
+        DependencyProvider.class,
+        SystemUIBinder.class,
+        SystemUIModule.class,
+        BiancaSystemUIModule.class})
+public interface BiancaSysUIComponent extends SysUIComponent {
+    @SysUISingleton
+    @Subcomponent.Builder
+    interface Builder extends SysUIComponent.Builder {
+        BiancaSysUIComponent build();
+    }
+}
---
+++ packages/SystemUI/src/com/bianca/systemui/dagger/BiancaSysUISubcomponentModule.java
@@ -0,0 +1,10 @@
+package com.bianca.systemui.dagger;
+
+import dagger.Module;
+
+/**
+ * Dagger module for including the WMComponent.
+ */
+@Module(subcomponents = {BiancaSysUIComponent.class})
+public abstract class BiancaSysUISubcomponentModule {
+}
