3d0e04b769783daf550ec895d68c70bf369b98f3
aswin7469 <aswinas@pixysos.com>
2022-04-06 20:04:55 +0530

    SystemUI: Add back charging ripple
    Change-Id: I78bb4b2934a792c34d820760c83b023514656296
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -468,6 +468,7 @@ public class CentralSurfacesImpl extends CoreStartable implements
     DozeServiceHost mDozeServiceHost;
     private boolean mWakeUpComingFromTouch;
     private LightRevealScrim mLightRevealScrim;
+    private WiredChargingRippleController mChargingRippleAnimationController;
     private PowerButtonReveal mPowerButtonReveal;
 
     private final Object mQueueLock = new Object();
@@ -863,6 +864,7 @@ public class CentralSurfacesImpl extends CoreStartable implements
         mDemoModeController = demoModeController;
         mNotificationIconAreaController = notificationIconAreaController;
         mBrightnessSliderFactory = brightnessSliderFactory;
+        mChargingRippleAnimationController = wiredChargingRippleController;
         mWallpaperController = wallpaperController;
         mOngoingCallController = ongoingCallController;
         mStatusBarSignalPolicy = statusBarSignalPolicy;
