03e81a09a6c8fdd35acaf981fb292bef3b6ada8e
Mocarafee <harif.rp05@gmail.com>
2023-03-16 22:39:41 +0700

    Revert "DozeSensors: only use proximity sensor if supported"
    This reverts commit 695cbeaa805286b24bf4e5a6f6f60d10b6da38ea.

--- packages/SystemUI/src/com/android/systemui/doze/DozeSensors.java
+++ packages/SystemUI/src/com/android/systemui/doze/DozeSensors.java
@@ -46,7 +46,6 @@ import com.android.internal.logging.UiEvent;
 import com.android.internal.logging.UiEventLogger;
 import com.android.internal.logging.UiEventLoggerImpl;
 import com.android.keyguard.KeyguardUpdateMonitor;
-import com.android.systemui.R;
 import com.android.systemui.biometrics.AuthController;
 import com.android.systemui.plugins.SensorManagerPlugin;
 import com.android.systemui.settings.UserTracker;
@@ -266,15 +265,13 @@ public class DozeSensors {
                         false /* requiresProx */,
                         true /* immediatelyReRegister */),
         };
-        if (context.getResources().getBoolean(R.bool.doze_proximity_sensor_supported)) {
-            setProxListening(false);  // Don't immediately start listening when we register.
-            mProximitySensor.register(
-                    proximityEvent -> {
-                        if (proximityEvent != null) {
-                            mProxCallback.accept(!proximityEvent.getBelow());
-                        }
-                    });
-        }
+        setProxListening(false);  // Don't immediately start listening when we register.
+        mProximitySensor.register(
+                proximityEvent -> {
+                    if (proximityEvent != null) {
+                        mProxCallback.accept(!proximityEvent.getBelow());
+                    }
+                });
 
         mDevicePostureController.addCallback(mDevicePostureCallback);
     }
