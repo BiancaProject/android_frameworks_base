7d4647038e17861d09f5ddce27b1f54d28bbcd26
Tim Zimmermann <tim@linux4.de>
2023-04-09 20:30:05 +0200

    SystemUI: Add settings to change QS column count [1/2]
    * Replace max tiles with max columns for round qs
      in order to properly animate qqs => qs transition
    
    Change-Id: Id77c0ee01d8a5d7ca3ac6d4167c1cf30d450bb4d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11301,6 +11301,30 @@ public final class Settings {
 
 
         /**
+         * Quicksettings columns
+         * @hide
+         */
+        public static final String QS_NUM_COLUMNS = "qs_num_columns";
+
+        /**
+         * Quicksettings columns landscape
+         * @hide
+         */
+        public static final String QS_NUM_COLUMNS_LANDSCAPE = "qs_num_columns_landscape";
+
+        /**
+         * Quick QS columns
+         * @hide
+         */
+        public static final String QQS_NUM_COLUMNS = "qqs_num_columns";
+
+        /**
+         * Quick QS columns landscape
+         * @hide
+         */
+        public static final String QQS_NUM_COLUMNS_LANDSCAPE = "qqs_num_columns_landscape";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
