a9e6ed78d38efa34a798597b533600278d390f15
Mocarafee <harif.rp05@gmail.com>
2023-12-18 04:20:46 +0000

    fixup! SystemUI: Add Autobrightness tile

--- packages/SystemUI/src/com/android/systemui/qs/tiles/AutoBrightnessTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/AutoBrightnessTile.java
@@ -51,6 +51,7 @@ import com.android.systemui.plugins.FalsingManager;
 import com.android.systemui.plugins.qs.QSTile.BooleanState;
 import com.android.systemui.plugins.statusbar.StatusBarStateController;
 import com.android.systemui.qs.QSHost;
+import com.android.systemui.qs.QsEventLogger;
 import com.android.systemui.qs.logging.QSLogger;
 import com.android.systemui.qs.tileimpl.QSTileImpl;
 import com.android.systemui.statusbar.policy.KeyguardStateController;
@@ -79,6 +80,7 @@ public class AutoBrightnessTile extends QSTileImpl<BooleanState> {
     @Inject
     public AutoBrightnessTile(
             QSHost host,
+            QsEventLogger uiEventLogger,
             @Background Looper backgroundLooper,
             @Main Handler mainHandler,
             FalsingManager falsingManager,
@@ -87,7 +89,7 @@ public class AutoBrightnessTile extends QSTileImpl<BooleanState> {
             ActivityStarter activityStarter,
             QSLogger qsLogger,
             KeyguardStateController keyguardStateController) {
-        super(host, backgroundLooper, mainHandler, falsingManager, metricsLogger,
+        super(host, uiEventLogger, backgroundLooper, mainHandler, falsingManager, metricsLogger,
                 statusBarStateController, activityStarter, qsLogger);
     }
 
